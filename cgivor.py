90564ba28e938a5165c9814687aec1d64a18fd74
Skyler Bruggink <skylerbruggink@gmail.com>
2022-08-03 15:48:34 -0400

    Update cgivor.py

--- cgivor.py
+++ cgivor.py
@@ -7,7 +7,7 @@ bpy.ops.preferences.addon_enable(module="cycles")
 # Get the containing folder of this file
 dir_path = os.path.dirname(os.path.realpath(__file__))
 # Load Config
-bpy.ops.cgivor.load_config(config_path=os.path.join(dir_path, "cgivor.yaml"))
+bpy.ops.cgivor.load_config(config_path=os.path.join(dir_path, "cgivor.json"))
 # Load Scene
 bpy.ops.cgivor.init_scene()
 # Run generator
