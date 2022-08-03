3fb4fe61587ad95f0850c5cfc5c823a06ed5442d
Skyler Bruggink <skylerbruggink@gmail.com>
2022-08-02 22:04:18 -0400

    Update install_cgivor_addon.py

--- install_cgivor_addon.py
+++ install_cgivor_addon.py
@@ -6,12 +6,3 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 addon_file = os.path.join(dir_path, "cgivor_addon.zip")
 bpy.ops.preferences.addon_install(overwrite=True, filepath=addon_file)
 bpy.ops.preferences.addon_enable(module="cgivor_addon")
-
-preferences = bpy.context.preferences
-cycles_preferences = preferences.addons['cycles'].preferences
-cuda_devices, opencl_devices = cycles_preferences.get_devices()
-
-cycles_preferences.compute_device_type = "CUDA"
-
-for device in cuda_devices:
-    device.use = True
